d1ca23bf8761ed6b47676b298da05f4bd3e47760
Boris <boris.rozzy@gmail.com>
2026-04-14 11:19:48 -0400

    feat: complete Core DSL builder implementation

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,14 @@
-import { GameDefinition } from "./types.js";
+import { GameDefinition, ActionDefinition, PhaseDefinition } from "./types.js";
 export { buildXStateMachine } from "./machine.js";
 
 export function defineGame<TState>(config: GameDefinition<TState>): GameDefinition<TState> {
+  return config;
+}
+
+export function createAction<TState, TPayload>(config: ActionDefinition<TState, TPayload>): ActionDefinition<TState, TPayload> {
+  return config;
+}
+
+export function createPhase<TState>(config: PhaseDefinition<TState>): PhaseDefinition<TState> {
   return config;
 }
