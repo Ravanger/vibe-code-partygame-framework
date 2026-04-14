421d955da2ef39a33b86ee31cd927ba79a01c3b0
Boris <boris.rozzy@gmail.com>
2026-04-14 16:17:37 -0400

    feat: integrate GameRoomState into GameClient

--- packages/client/src/GameClient.ts
+++ packages/client/src/GameClient.ts
@@ -1,10 +1,17 @@
-import { ConnectionStatus } from "./types.js";
+import { GameRoomState } from './state.svelte.js';
+import type { ConnectionStatus } from './types.js';
 
 export class GameClient {
   public connectionStatus: ConnectionStatus = 'connecting';
+  public state = new GameRoomState();
   private roomCode: string;
-  
+
   constructor(options: { roomCode: string }) {
     this.roomCode = options.roomCode;
+  }
+
+  // Hook for Colyseus room
+  onStateChange(serverState: any) {
+    this.state.sync(serverState);
   }
 }
